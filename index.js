21801e5f331f858614b87d5e60835da641728d9a
Subash Nagarkoti <subashnagarkoti@Subashs-MacBook-Air.local>
2025-07-30 15:46:06 +0545

    adding new branch and creating PR

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logMessage = `Server started at ${moment().format()}\n`;
 fs.appendFileSync('app.log', logMessage);  // creates app.log
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, World!\n');
+  res.end('Hello, World! Testing git PR and merge \n');
 });
 
 server.listen(3000, () => {
